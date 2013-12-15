c68b16aa8252e9aa533666e08989eb1e11218ece
Viktor Hapiak <gorz1994@gmail.com>
2013-12-15 16:42:22 +0200

    add installer

---
+++ Install.bat
@@ -0,0 +1,10 @@
+@java -version 1>nul 2>&1 && (
+	@set JAVA=
+	goto RUN
+) || (
+	@for /f %%j in ("java.exe") do @set JAVA=%%~dp$PATH:j
+	goto RUN
+)
+:RUN
+start %JAVA%java -Dfile.encoding=UTF-8 -cp .\mysql-connector.jar;.\build\classes\ installer.Installer
+exit
