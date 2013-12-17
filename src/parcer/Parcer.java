8c2338916e5354b1092877be0b1bc361ed981e4e
Kirill Mirkin <kirill.mirkin@gmail.com>
2013-12-17 12:26:27 +0200

    add db update

--- src/parcer/Parcer.java
+++ src/parcer/Parcer.java
@@ -25,40 +25,19 @@ public class Parcer {
 
     private static final Logger LOG = Logger.getLogger(Parcer.class.getName());
 
-    private ArrayList<CarBrand> brandsList;
-    private ArrayList<CarBody> bodiesList;
-    private ArrayList<CarCategory> categoriesList;
-
-    private HashMap<CarModel, ArrayList<CarEdition>> editionsHashMap;
-    private HashMap<CarBrand, ArrayList<CarModel>> modelsHashMap;
-    private HashMap<CarModel, ArrayList<CarComponent>> componentsHashMap;
-    private HashMap<CarModel, ArrayList<CarComponent>> equipmentsHashMap;
-    private HashMap<CarModel, ArrayList<CarFactor>> yearFactorsHashMap;
-
-    private String _brandName;
-    private String _modelName;
-    private String _bodyName;
-    private String _materialName;
-    private String _componentName;
-    private String _categoryName;
-    private int _idModel = 0;
-    private int _idEdition = 0;
-    private float _repairCost;
-    private float _volume;
-    private float _price;
-    private int _year;
-    private float _wearFactor;
-    private float _region1;
-    private float _region2;
-    private float _region3;
-    private boolean _flag;
-
     private final String path;
     private final Statement statement;
-    private int _idComponent = 0;
-    private int _idEquipment = 0;
-    private String _equipmentName;
-    private float _factor;
+
+    private final ArrayList<CarBrand> brandsList;
+    private final ArrayList<CarBody> bodiesList;
+    private final ArrayList<CarCategory> categoriesList;
+    private final ArrayList<CarComponent> materialsList;
+
+    private final HashMap<CarModel, ArrayList<CarEdition>> editionsHashMap;
+    private final HashMap<CarBrand, ArrayList<CarModel>> modelsHashMap;
+    private final HashMap<CarModel, ArrayList<CarComponent>> componentsHashMap;
+    private final HashMap<CarModel, ArrayList<CarComponent>> equipmentsHashMap;
+    private final HashMap<CarModel, ArrayList<CarFactor>> yearFactorsHashMap;
 
     public Parcer(String path, Statement statement) {
         this.path = path;
@@ -66,12 +45,13 @@ public class Parcer {
         bodiesList = new ArrayList<>();
         brandsList = new ArrayList<>();
         categoriesList = new ArrayList<>();
+        materialsList = new ArrayList<>();
         editionsHashMap = new HashMap<>();
         modelsHashMap = new HashMap<>();
         componentsHashMap = new HashMap<>();
         equipmentsHashMap = new HashMap<>();
         yearFactorsHashMap = new HashMap<>();
-        
+
     }
 
     public void startParcer() {
@@ -85,9 +65,10 @@ public class Parcer {
     }
 
     public void MaterialsParcer() {
+        String _materialName = null;
+        float _price = 0;
         InputStream in = null;
         try {
-//            in = new FileInputStream("/home/afterthegame/BrandModelEditionBody.xls");
             in = new FileInputStream(path + "/MATERIALS.xls");
             HSSFWorkbook wb = new HSSFWorkbook(in); //Получаем workbook
             HSSFSheet sheet = wb.getSheetAt(0); // Проверяем только первую страницу
@@ -118,11 +99,11 @@ public class Parcer {
                                 tmp = cell.getStringCellValue();
                                 _price = Float.valueOf(tmp);
                             }
-//                            statement.executeUpdate("INSERT INTO materials(name, price) VALUES (" 
-//                                    + _materialName + "," + _price + ");");
                             break;
                     }
                 }
+                CarComponent material = new CarComponent(0, _materialName, _price, null);
+                materialsList.add(material);
             }
         } catch (FileNotFoundException ex) {
             Logger.getLogger(Parcer.class.getName()).log(Level.SEVERE, null, ex);
@@ -138,9 +119,10 @@ public class Parcer {
     }
 
     public void CategoryParcer() {
+        String _categoryName = null;
+        boolean _flag = false;
         InputStream in = null;
         try {
-//            in = new FileInputStream("/home/afterthegame/BrandModelEditionBody.xls");
             in = new FileInputStream(path + "/CATEGORIES.xls");
             HSSFWorkbook wb = new HSSFWorkbook(in); //Получаем workbook
             HSSFSheet sheet = wb.getSheetAt(0); // Проверяем только первую страницу
@@ -186,11 +168,15 @@ public class Parcer {
     }
 
     public void ComponentsParcer() {
+        String _brandName = null;
+        String _modelName = null;
+        String _componentName = null;
         InputStream in = null;
+        float _price = 0;
+        int _idComponent = 0;
         CarModel _Model;
         CarBrand _Brand;
         try {
-//            in = new FileInputStream("/home/afterthegame/BrandModelEditionBody.xls");
             in = new FileInputStream(path + "/COMPONENTS.xls");
             HSSFWorkbook wb = new HSSFWorkbook(in); //Получаем workbook
             HSSFSheet sheet = wb.getSheetAt(0); // Проверяем только первую страницу
@@ -279,13 +265,18 @@ public class Parcer {
             }
         }
     }
-    
+
     public void EquipmentsParcer() {
+        String _brandName = null;
+        String _modelName = null;
+        String _componentName = null;
+        String _equipmentName = null;
         InputStream in = null;
+        float _price = 0;
+        int _idEquipment = 0;
         CarModel _Model;
         CarBrand _Brand;
         try {
-//            in = new FileInputStream("/home/afterthegame/BrandModelEditionBody.xls");
             in = new FileInputStream(path + "/EQUIPMENTS.xls");
             HSSFWorkbook wb = new HSSFWorkbook(in); //Получаем workbook
             HSSFSheet sheet = wb.getSheetAt(0); // Проверяем только первую страницу
@@ -374,13 +365,16 @@ public class Parcer {
             }
         }
     }
-    
+
     public void YearFactorParcer() {
+        String _brandName = null;
+        String _modelName = null;
         InputStream in = null;
+        float _factor = 0;
+        int _year = 0;
         CarModel _Model;
         CarBrand _Brand;
         try {
-//            in = new FileInputStream("/home/afterthegame/BrandModelEditionBody.xls");
             in = new FileInputStream(path + "/YEARFACTOR.xls");
             HSSFWorkbook wb = new HSSFWorkbook(in); //Получаем workbook
             HSSFSheet sheet = wb.getSheetAt(0); // Проверяем только первую страницу
@@ -414,7 +408,7 @@ public class Parcer {
                             break;
                         case 2:
                             _year = (int) cell.getNumericCellValue();
-                            LOG.info(""+_year);
+                            LOG.info("" + _year);
                             break;
                         case 3:
                             if (cell.getCellType() == Cell.CELL_TYPE_NUMERIC) {
@@ -471,13 +465,26 @@ public class Parcer {
     }
 
     public void BrandModelEditionBodyCategoryRegionWearRepairParcer() {
+        String _brandName = null;
+        String _modelName = null;
+        String _bodyName = null;
+        String _categoryName = null;
+        int _idModel = 0;
+        int _idEdition = 0;
+        float _repairCost = 0;
+        float _volume = 0;
+        float _price = 0;
+        int _year = 0;
+        float _wearFactor = 0;
+        float _region1 = 0;
+        float _region2 = 0;
+        float _region3 = 0;
         InputStream in = null;
         CarBrand _Brand;
         CarModel _Model;
         CarCategory _Category;
         CarBody _Body;
         try {
-//            in = new FileInputStream("/home/afterthegame/auto.xls");
             in = new FileInputStream(path + "/auto.xls");
 //            Получаем workbook
             HSSFWorkbook wb = new HSSFWorkbook(in);
@@ -669,14 +676,113 @@ public class Parcer {
         }
     }
 
-    public void UpdateDatabaseAfterParce(){
-        
+    public void UpdateDatabaseAfterParce() {
+        try {
+// --------------------------------ТАБЛИЦА МАТЕРИАЛЫ---------------------------------           
+            statement.executeUpdate("DELETE * FROM materials;");
+            for (CarComponent material : materialsList) {
+                statement.addBatch("INSERT INTO materials(name, price) VALUES ("
+                        + material.getName() + "," + material.getPrice() + ");");
+            }
+            statement.executeBatch();
+
+// --------------------------------ТАБЛИЦА КАТЕГОРИИ---------------------------------
+            statement.executeUpdate("DELETE * FROM categories;");
+            for (CarCategory category : categoriesList) {
+                String f;
+                if (category.isFlag() == true) {
+                    f = "TRUE";
+                } else {
+                    f = "FALSE";
+                }
+                statement.addBatch("INSERT INTO categories(id, name, flag) VALUES ("
+                        + category.getId() + "," + category.getName() + "," + f + ");");
+            }
+            statement.executeBatch();
+
+// --------------------------------ТАБЛИЦА КУЗОВА---------------------------------
+            statement.executeUpdate("DELETE * FROM bodies;");
+            for (CarBody body : bodiesList) {
+                statement.addBatch("INSERT INTO bodies(id, name) VALUES ("
+                        + body.getId() + "," + body.getName() + ");");
+            }
+            statement.executeBatch();
+
+// --------------------------------ТАБЛИЦА МАРОК---------------------------------
+            statement.addBatch("DELETE * FROM brands;");
+            statement.addBatch("DELETE * FROM models;");
+            statement.addBatch("DELETE * FROM edititons;");
+            statement.addBatch("DELETE * FROM equipments;");
+            statement.addBatch("DELETE * FROM components;");
+            statement.addBatch("DELETE * FROM year_factors;");
+            statement.addBatch("DELETE * FROM wear_factors;");
+            statement.addBatch("DELETE * FROM repair_cost;");
+            statement.addBatch("DELETE * FROM region_factors;");
+            statement.executeBatch();
+
+            for (CarBrand brand : brandsList) {
+                statement.addBatch("INSERT INTO brands(id, name) VALUES ("
+                        + brand.getId() + "," + brand.getName() + ");");
+
+//                 --------------------------------ТАБЛИЦА МOДЕЛЕЙ---------------------------------
+                ArrayList<CarModel> _currentCarModelsList = modelsHashMap.get(brand);
+                for (CarModel model : _currentCarModelsList) {
+                    statement.addBatch("INSERT INTO models(id, name, brand_id, wear_factor, year_factor, region_factor, category_id)"
+                            + " VALUES ("
+                            +model.getId()+","+ model.getName() + "," + brand.getId() + "," + model.getId()
+                            + "," + model.getId() + "," + model.getId() + "," + model.getCategory() + ");");
+
+//                      --------------------------------ТАБЛИЦА ИЗНОСА---------------------------------
+                    statement.addBatch("INSERT INTO wear_factors(id, val) "
+                            + " VALUES (" + model.getId() + "," + model.getWearFactor() + ");");
+
+//                      --------------------------------ТАБЛИЦА ЦЕНА РЕМОНТА---------------------------------
+                    statement.addBatch("INSERT INTO repair_cost(id, val, model_id) "
+                            + " VALUES (" + model.getId() + ","+ model.getRepairCost() + "," + model.getId() + ");");
+
+//                      --------------------------------ТАБЛИЦА ФАКТОР РЕГИОНА---------------------------------
+                    statement.addBatch("INSERT INTO region_factors(id,low, high, another) "
+                            + " VALUES (" + model.getId() + ","+ model.getRegionFactor1() + "," + model.getRegionFactor2() + "," + model.getRegionFactor3() + ");");
+                    ArrayList<CarEdition> _currentCarEditionsList = editionsHashMap.get(model);
+
+//                      --------------------------------ТАБЛИЦА ВЫПУСКОВ---------------------------------
+                    for (CarEdition edition : _currentCarEditionsList) {
+                        statement.addBatch("INSERT INTO editions (id, year, volume, price, model_id, body_id) VALUES ("
+                                + edition.getId() + ","+ edition.getYear() + "," + edition.getVolume() + "," + edition.getPrice()
+                                + "," + model.getId() + "," + brand.getId() + ");");
+                    }
+
+//                      --------------------------------ТАБЛИЦА КОМПОНЕНТ---------------------------------
+                    ArrayList<CarComponent> _currentCarComponentList = componentsHashMap.get(model);
+                    for (CarComponent component : _currentCarComponentList) {
+                        statement.addBatch("INSERT INTO components(id, name, price, model_id) VALUES ("
+                                + component.getId() + ","+ component.getName() + "," + component.getPrice() + "," + model.getId() + ");");
+                    }
+
+//                      --------------------------------ТАБЛИЦА ОБОРУДОВАНИЯ---------------------------------
+                    ArrayList<CarComponent> _currentCarEquipmentList = equipmentsHashMap.get(model);
+                    for (CarComponent equipment : _currentCarEquipmentList) {
+                        statement.addBatch("INSERT INTO equipments(id, name, price, model_id) VALUES ("
+                                + equipment.getId() + ","+ equipment.getName() + "," + equipment.getPrice() + "," + equipment.getId() + ");");
+                    }
+//                      --------------------------------ТАБЛИЦА ФАКТОР ГОДА---------------------------------
+                    ArrayList<CarFactor> _currentCarFactorsList = yearFactorsHashMap.get(model);
+                    for (CarFactor factor : _currentCarFactorsList) {
+                        statement.addBatch("INSERT INTO year_factors(id, year, val) VALUES ("
+                                + factor.getId() + "," + factor.getYear() + "," + factor.getFactor() + ");");
+                    }
+                }
+                statement.executeBatch();
+            }
+
+// ----------------------------------------------------------------------------------             
+        } catch (SQLException ex) {
+            Logger.getLogger(Parcer.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
+
     public static void main(String[] args) {
         Parcer p = new Parcer("/home/afterthegame", null);
         p.startParcer();
-//        p.BrandModelEditionBodyCategoryRegionWearRepairParcer();
-//        Sort.SortModel(p.getArrayBrandModelEditionBodys());
-//            p.Print();
     }
 }
